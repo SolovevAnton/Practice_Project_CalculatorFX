7ebe5c5e012184e3efc018bed2e62af0edce6f29
Solovev Anton <solovev.anton93@gmail.com>
2023-05-18 17:27:08 +0300

    added methods for buttons

--- src/main/java/com/solovev/calculatorfx/App.java
+++ src/main/java/com/solovev/calculatorfx/App.java
@@ -13,7 +13,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Calculator");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/solovev/calculatorfx/controllers/MainController.java
+++ src/main/java/com/solovev/calculatorfx/controllers/MainController.java
@@ -1,4 +1,48 @@
 package com.solovev.calculatorfx.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.util.function.BiFunction;
+
 public class MainController {
+    @FXML
+    public TextField textFieldInput1;
+    @FXML
+    public TextField textFieldOperation;
+    @FXML
+    public TextField textFieldInput2;
+    @FXML
+    public TextField textFieldResult;
+
+    /**
+     * Method performs given operation to the inputs, writes result to result, and operation sign to OperationFiled
+     * @param function function to perform
+     * @param functionSign sign of the function
+     */
+    private void operation(BiFunction<Double,Double,Double> function,String functionSign){
+    Double input1 = Double.parseDouble(textFieldInput1.getText());
+    Double input2 = Double.parseDouble(textFieldInput2.getText());
+    textFieldResult.setText(function.apply(input1,input2).toString());
+    textFieldOperation.setText(functionSign);
+    }
+    @FXML
+    public void buttonPlus(ActionEvent actionEvent) {
+        operation(Double::sum,"+" );
+    }
+    @FXML
+    public void buttonMinus(ActionEvent actionEvent) {
+        operation((i, k) -> i - k, "-");
+    }
+    @FXML
+    public void buttonMultiply(ActionEvent actionEvent) {
+        operation((i,k)-> i*k,"x" );
+    }
+    @FXML
+    public void buttonDivide(ActionEvent actionEvent) {
+        operation((i,k)-> i/k,"/" );
+    }
+
 }
