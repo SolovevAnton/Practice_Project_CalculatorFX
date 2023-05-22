985a0259318c10c4479f1dd4ec6a89610d675478
Solovev Anton <solovev.anton93@gmail.com>
2023-05-22 12:56:43 +0300

    added label updated UI

--- src/main/java/com/solovev/calculatorfx/controllers/MainController.java
+++ src/main/java/com/solovev/calculatorfx/controllers/MainController.java
@@ -2,6 +2,8 @@ package com.solovev.calculatorfx.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.util.function.BiFunction;
@@ -15,33 +17,54 @@ public class MainController {
     public TextField textFieldInput2;
     @FXML
     public TextField textFieldResult;
+    @FXML
+    public Label labelWarningText;
+    private String boundaries = String.format("%s from: %s to %s", Double.class.getSimpleName(), Double.MIN_VALUE, Double.MAX_VALUE);
+    @FXML
+    public void initialize(){
+        String boundaries = String.format("%s from: %s to %s", Double.class.getSimpleName(), Double.MIN_VALUE, Double.MAX_VALUE);
+        labelWarningText.setText(labelWarningText.getText() + boundaries);
+    }
 
+    //TODO ? Should number format exc be caught here?
     /**
      * Method performs given operation to the inputs, writes result to result, and operation sign to OperationFiled
-     * @param function function to perform
+     *
+     * @param function     function to perform
      * @param functionSign sign of the function
      */
-    private void operation(BiFunction<Double,Double,Double> function,String functionSign){
-    Double input1 = Double.parseDouble(textFieldInput1.getText());
-    Double input2 = Double.parseDouble(textFieldInput2.getText());
-    textFieldResult.setText(function.apply(input1,input2).toString());
-    textFieldOperation.setText(functionSign);
+    private void operation(BiFunction<Double, Double, Double> function, String functionSign) {
+        try {
+            Double input1 = Double.parseDouble(textFieldInput1.getText());
+            Double input2 = Double.parseDouble(textFieldInput2.getText());
+            textFieldResult.setText(function.apply(input1, input2).toString());
+            textFieldOperation.setText(functionSign);
+        } catch (NumberFormatException e){
+            Alert formatAlert = new Alert(Alert.AlertType.ERROR);
+            formatAlert.setHeaderText("Wrong format");
+            formatAlert.setContentText("Supports only numbers " + boundaries);
+            formatAlert.show();
+        }
     }
+
     @FXML
     public void buttonPlus(ActionEvent actionEvent) {
-        operation(Double::sum,"+" );
+        operation(Double::sum, "+");
     }
+
     @FXML
     public void buttonMinus(ActionEvent actionEvent) {
         operation((i, k) -> i - k, "-");
     }
+
     @FXML
     public void buttonMultiply(ActionEvent actionEvent) {
-        operation((i,k)-> i*k,"x" );
+        operation((i, k) -> i * k, "x");
     }
+
     @FXML
     public void buttonDivide(ActionEvent actionEvent) {
-        operation((i,k)-> i/k,"/" );
+        operation((i, k) -> i / k, "/");
     }
 
 }
